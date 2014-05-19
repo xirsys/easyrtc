1561287b72a21b741241c4a6c3b8ef83ec0642b5
Alex Thomas <alex@xirsys.com>
2014-05-19 15:37:26 +0000

    added a custom call to xirsys's stun and turn servers

--- server_example/server.js
+++ server_example/server.js
@@ -4,6 +4,9 @@ var express = require("express");           // web framework external module
 var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
 
+// Add request module to call XirSys servers
+var request = require("request");
+
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
 
@@ -14,6 +17,33 @@ var webServer = http.createServer(httpApp).listen(8080);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
+
+easyrtc.on("getIceConfig", function(connectionObj, callback) {
+  
+    // This object will take in an array of XirSys STUN and TURN servers
+    // and override the original iceConfig object
+    var iceConfig = [];
+
+    request.post('https://api.xirsys.com/getIceServers', {
+        form: {
+            ident: "< Your username >",
+            secret: "< Your secret API token >",
+            domain: "< www.yourdomain.com >",
+            application: "default",
+            room: "default",
+            secure: 1
+        },
+        json: true
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // data.d.iceServers is where the array of ICE servers lives
+            iceConfig = body.d.iceServers;  
+            console.log(iceConfig);
+            callback(null, iceConfig);
+        }
+    });
+});
 
 // Start EasyRTC server
 var rtc = easyrtc.listen(httpApp, socketServer);
